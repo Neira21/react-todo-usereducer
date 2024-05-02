1090a5b5210af97f565f9c8ee8f4325af4f117d6
Alvaro Neira <alvaroneira98@gmail.com>
2024-05-01 19:45:17 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://neira21.github.io/react-todo-usereducer'
 })
